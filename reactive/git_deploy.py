6de0af42196e1131f8add2b5ff0ffc1dff4d6693
bdx <jamesbeedy@gmail.com>
2016-09-06 16:38:00 -0700

    update layer to preform the clone and update_to_commit functions

--- reactive/git_deploy.py
+++ reactive/git_deploy.py
@@ -1,11 +1,29 @@
 from charms.reactive import when_not, set_state
+from charmhelpers.core.hookenv import config
 
-@when_not('git.deploy.available')
+from charms.layer.git_deploy import clone, update_to_commit
+
+from charms.layer import options
+
+
+@when_not('codebase.available')
 def git_deploy_avail():
-    """Set git.deploy.available state so
-    other layers know when they can use the
-    git, clone, and update_to_commit utility 
-    functions.
+    """Clone down codebase and set codebase.available state
     """
-    set_state('git.deploy.available')
+
+    opts = options('git-deploy')
+    # Check if path exists, clone down repo
+    if os.path.exists(opts('target')):
+        shutil.rmtree(opts('target'), ignore_errors=True)
+    clone()
+
+    # Update to commit if config('commit')
+    if config('commit') is not None:
+        update_to_commit()
+
+    # Chownr app-dir
+    chownr(path=opts('target'), owner=opts('owner'), group=opts('group'))
+
+    # Set codebase.available state
+    set_state('codebase.available')
 
